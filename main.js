0265cf1dc2a525377c8b730212040c83e3a7594b
Veer Singh <62992224+Purefekt@users.noreply.github.com>
2023-05-04 03:00:47 -0700

    data is shown in tables for first page of data, need to implement pagination

--- main.js
+++ main.js
@@ -23,7 +23,7 @@ function search_yelp(inp_keyword, inp_distance, inp_category, inp_page_num, inp_
 
 function search_box_input(data, keyword, distance, category, page_num, location) {
     var div_no_result = document.getElementById("no_result");
-    var div_table = document.getElementById("data_table");
+    var div_table = document.getElementById("data_table_container");
 
     var response = JSON.parse(data);
 
@@ -35,10 +35,74 @@ function search_box_input(data, keyword, distance, category, page_num, location)
         // else hide no records found message and show and build the table
         div_no_result.style.display = "none";
         div_table.style.display = "block";
-        build_table(data, keyword, distance, category, page_num, location);
+        build_table(response, keyword, distance, category, page_num, location);
     }
 }
 
 function build_table(data, keyword, distance, category, page_num, location) {
+
+    var table = document.getElementById("data_table");
+    var pagination = document.getElementById("pagination")
+
+    // reset table on every call
+    table.innerHTML = "<thead><tr><th scope='col' class='text-center'>Image</th><th scope='col' class='text-center'>Name</th><th scope='col' class='text-center'>Rating</th><th scope='col' class='text-center'>Distance</th></tr></thead>"
+
+    // build table rows based on number of results
+    num_results = data.length;
+    for (var i=0; i<num_results; i++) {
+        var id = data[i]["id"];
+        var name = data[i]["name"];
+        var image_url = data[i]["image_url"];
+        var rating = data[i]["rating"];
+        var distance = data[i]["distance"];
+        var count = i + 1;
+
+        var new_row = document.createElement("tr");
+        new_row.innerHTML = "<td><img class='img-fluid' style='max-height: 100px; max-width: 100px;'  src='" + image_url + "'></td><td class='text-center'>" + name + "</td><td class='text-center'>" + rating + "</td><td class='text-center'>" + distance + "</td>";
+
+        table.appendChild(new_row);
+    }
+
+    // pagination bar. If it is the first page, keep Previous disabled. If we dont have results for next page, keep Next disabled.
+    var pagination_ul = document.createElement("ul");
+    pagination_ul.setAttribute("class", "pagination");
+    pagination.appendChild(pagination_ul)
+
+    const previous_li = document.createElement("li");
+    if (page_num == 0) {
+        previous_li.setAttribute("class", "page-item disabled");
+        
+        const span = document.createElement("span");
+        span.setAttribute("class", "page-link");
+        span.textContent = "Previous";
+        
+        previous_li.appendChild(span);
+    } else {
+        previous_li.setAttribute("class", "page-item");
+        
+        const span = document.createElement("span");
+        span.setAttribute("class", "page-link");
+        span.textContent = "Previous";
+        
+        previous_li.appendChild(span);
+    }
+    pagination_ul.appendChild(previous_li)
+
+    var page_x_of_y_li = document.createElement("li");
+    page_x_of_y_li.innerHTML = "<li class='page-item disabled'><span class='page-link'>Page " + `${page_num+1}` + "</span></li>"
+    pagination_ul.appendChild(page_x_of_y_li)
+
+    const next_li = document.createElement("li");
+    next_li.setAttribute("class", "page-item");
+    const span = document.createElement("span");
+    span.setAttribute("class", "page-link");
+    span.textContent = "Next";
+    next_li.appendChild(span);
+    pagination_ul.appendChild(next_li);
+
+
+
+    window.localStorage.href = "#data_table_container";
+
 
 }
